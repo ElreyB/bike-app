c58f1987dca5fced3ed93175524f40863d4236da
Elrey Belmonti and Megan Olson <megan@>
2017-10-19 09:30:46 -0700

    create byLocation function, HTML layout and interface

--- js/bike-app-interface.js
+++ js/bike-app-interface.js
@@ -1,1 +1,11 @@
 import { BikeApp } from './../js/bike-app.js';
+
+$(document).ready(function(){
+  let bike = new BikeApp();
+  $(".location").submit(function(e){
+    e.preventDefault();
+    const userLocation = $("input.location").val();
+    $(".results").removeClass("hide");
+    bike.byLocation(userLocation);
+    });
+});
--- js/bike-app.js
+++ js/bike-app.js
@@ -1,5 +1,16 @@
 export class BikeApp {
   constructor(){
-    
+  }
+
+  byLocation(location){
+    $.get(`https://bikeindex.org:443/api/v3/search?page=1&per_page=25&location=${location}&distance=10&stolenness=stolen`)
+    .then(function(response){
+      let bikes = response.bikes;
+      bikes.forEach(function(bike){
+        $('#stolenBikes').append(`<li>${bike.title}</li>`);
+      });
+    }).fail(function(error){
+      $('.errors').text(`There was an error processing your request: ${error.responseText}. Please try again.`);
+    });
   }
 }
