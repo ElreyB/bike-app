f525f5f54ebcd5194b3408f64ddda99e3f5d6840
Elrey Belmonti and Megan Olson <megan@>
2017-10-19 10:40:09 -0700

    add date and helper function to convert to readable date

--- js/bike-app.js
+++ js/bike-app.js
@@ -1,13 +1,19 @@
+function convertDate(date){
+  let newDate = new Date(date * 1000);
+  let convertedDate = (newDate.getMonth() + 1) + "/" + (newDate.getDay() + 1) + "/" + newDate.getFullYear();
+  return convertedDate;
+}
+
 export class BikeApp {
   constructor(){
   }
 
   byLocation(location){
-    $.get(`https://bikeindex.org:443/api/v3/search?page=1&per_page=25&location=${location}&distance=10&stolenness=stolen`)
+    $.get(`https://bikeindex.org:443/api/v3/search?page=1&per_page=50&location=${location}&distance=10&stolenness=proximity`)
     .then(function(response){
       let bikes = response.bikes;
       bikes.forEach(function(bike){
-        $('#stolenBikes').append(`<li>${bike.title}</li>`);
+        $('#stolenBikes').append(`<li>${bike.title} : ${convertDate(bike.date_stolen)}</li>`);
       });
     }).fail(function(error){
       $('.errors').text(`There was an error processing your request: ${error.responseText}. Please try again.`);
