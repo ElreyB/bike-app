a8191e62bec514e5ec6739650dac1d200e8434e0
Elrey Belmonti and Megan Olson <megan@>
2017-10-19 13:16:15 -0700

    add loading icon

--- js/bike-app-interface.js
+++ js/bike-app-interface.js
@@ -1,30 +1,21 @@
 import { BikeApp } from './../js/bike-app.js';
 
-
-function loadTimer() {
-  let timer = setTimeout(showTimer(), 3000);
-}
-
-function loadResults() {
-  let timer = setTimeout(showResults(), 5000);
-}
-
-function showResults() {
-  $(".results").show();
-  $(".loader").hide();
-}
-
-function showTimer() {
-  $(".loader").show();
-}
-
 $(document).ready(function(){
   let bike = new BikeApp();
+
+  $(document).ajaxStart(function(){
+       $("#wait").css("display", "block");
+   });
+   $(document).ajaxComplete(function(){
+       $("#wait").css("display", "none");
+       $(".results").show();
+   });
+
   $(".location").submit(function(e){
     e.preventDefault();
-    loadResults();
-    loadTimer();
+    $(".results").hide();
     $("ul#stolenBikes").empty();
+
     const userLocation = $("input.location").val();
     const userAmount = $("input.amount").val();
     bike.byLocation(userLocation, userAmount);
